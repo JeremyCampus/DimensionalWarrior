9fd94ee58694ea69fcb759eb13b8e500c8869f93
Jérémy Laurens <jeremy.laurens@Digital-Grenoble.local>
2018-01-18 10:23:48 +0100

    Premier Push

---
+++ students/warrior.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . "/../base/localWarrior.php";
+
+
+$GLOBALS['warriorID'] = 'Ezranar';
+
+// Définissez vos class de combattants
+abstract class Warrior extends LocalWarrior {
+
+};
