f0c9a15a16e8b4b91bfa526c2baa030f673a38f3
Jérémy Laurens <jeremy.laurens@Digital-Grenoble.local>
2018-01-18 15:43:19 +0100

    All green

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,28 @@
 
   class BattleField extends BaseBattleField
   {
+    public static function createMyWarrior()
+    {
+      $Lloyd = new MarvelWarrior("Ezranar");
+      $Lloyd->SetImageUrl("http://vignette3.wikia.nocookie.net/aselia/images/2/2e/Jude_Cut-in_(ToX2).png/revision/latest?cb=20140503053956");
 
+      $sabres = new Weapon ("2 Sabres",12000);
+      $sabres->SetImageUrl("http://2.bp.blogspot.com/-avvu5ApbONc/Ty5N3VETc3I/AAAAAAAACzs/j5IZDgioHk0/s1600/sabreentier.jpg");
 
+      $Lloyd->setWeapon($sabres);
+      $Lloyd->saveNew();
+
+    }
+    public static function createOtherWarrior()
+    {
+      $squelettes = new PokemonWarrior("Big Boss Squelette");
+      $squelettes->SetImageUrl("https://vignette.wikia.nocookie.net/aselia/images/e/e5/Sword_Dancer_Model.png/revision/latest?cb=20170805032315");
+
+      $epeeRouille = new Weapon ("2 Sabres",120);
+      $epeeRouille->SetImageUrl("https://vignette.wikia.nocookie.net/mogapedia/images/2/28/Grande_%C3%A9p%C3%A9e_us%C3%A9e.png/revision/latest?cb=20140516174413&path-prefix=fr");
+
+      $squelettes->setWeapon($epeeRouille);
+      $squelettes->saveNew();
+
+    }
   }
--- students/warrior.php
+++ students/warrior.php
@@ -7,5 +7,101 @@ $GLOBALS['warriorID'] = 'Ezranar';
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
+  public $id;
+  public $name;
+  public $speed;
+  public $life;
+  public $shield;
+  public $imageUrl;
+  public $weapon;
 
-};
+  public function __construct($newId) {
+    $this->id=$newId;
+    $this->speed=30;
+    $this->life=100;
+    $this->shield=20;
+  }
+
+  public function SetWeapon($myWeapon)
+  {
+    $this->weapon = $myWeapon;
+  }
+  public function getWeapon () {
+      return $this->weapon;
+  }
+
+  public function SetImageUrl($url)
+  {
+    $this->imageUrl = $url;
+  }
+
+}
+
+
+// $spoke = new StartrekWarrior("toto");
+//
+// $sabre = new Weapon ()
+//
+// $spoke->setWeapon($sabre);
+
+
+class StartrekWarrior extends Warrior
+{
+  public $mentalPower;
+  public function power()
+  {
+    return $this->mentalPower;
+  }
+  public function __construct($newId) //declare $newId
+  {
+     parent::__construct($newId);//utilise $newId
+     $this->mentalPower=8;
+  }
+
+}
+
+class MarvelWarrior  extends Warrior
+{
+  public $superPower ;
+  public function power()
+  {
+    return $this->superPower;
+  }
+  public function __construct($newId) //declare $newId
+  {
+     parent::__construct($newId);//utilise $newId
+     $this->superPower=100;
+  }
+}
+
+class PokemonWarrior  extends Warrior
+{
+  public $level;
+  public function power()
+  {
+    return $this->level;
+  }
+  public function __construct($newId) //declare $newId
+  {
+     parent::__construct($newId);//utilise $newId
+     $this->level=1;
+  }
+}
+
+
+class weapon
+{
+  public $id;
+  public $strength;
+  public $imageUrl;
+
+  public function __construct($newId,$newStrength) //declare $newId
+  {
+    $this->id=$newId;
+    $this->strength = $newStrength;
+  }
+  public function SetImageUrl($url)
+  {
+    $this->imageUrl = $url;
+  }
+}
